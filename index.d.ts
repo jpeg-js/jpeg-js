ca276011b6c974a5c488bc48ca0f8edb9d505069
Florian Keller <ffflorian@users.noreply.github.com>
2020-07-02 22:42:49 +0200

    fix(types): add comments property to decode output (#72)

--- index.d.ts
+++ index.d.ts
@@ -22,7 +22,7 @@ export declare function decode(
     maxResolutionInMP?: number;
     maxMemoryUsageInMB?: number;
   },
-): UintArrRet;
+): UintArrRet & {comments?: string[]};
 export declare function decode(
   jpegData: BufferLike,
   opts?: {
@@ -33,4 +33,4 @@ export declare function decode(
     maxResolutionInMP?: number;
     maxMemoryUsageInMB?: number;
   },
-): BufferRet;
+): BufferRet & {comments?: string[]};
