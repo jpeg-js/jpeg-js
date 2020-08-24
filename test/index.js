5e8ec273984b2f491d399c92de42e3bf9d915fba
Vincent Dufrasnes <Chupsy@users.noreply.github.com>
2020-08-24 18:07:31 +0200

    feat(decoder): add FFDC marker support (#79)

--- test/index.js
+++ test/index.js
@@ -223,6 +223,14 @@ it('should be able to encode/decode image with exif data', function () {
   expect(loopImageData.exifBuffer).toEqual(imageData.exifBuffer);
 });
 
+it('should be to decode image with ffdc marker', function () {
+  var jpegData = fixture('marker-ffdc.jpg');
+  var imageData = jpeg.decode(new Uint8Array(jpegData));
+  expect(imageData.height).toEqual(200);
+  expect(imageData.width).toEqual(200);
+});
+
+
 it('should be able to decode large images within memory limits', () => {
   var jpegData = fixture('black-6000x6000.jpg');
   var rawImageData = jpeg.decode(jpegData);
